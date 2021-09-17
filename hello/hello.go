194c66208a971733394f91345ac3bf6c6f5f38c3
Flayshon <flayshon@gmail.com>
2021-09-16 21:24:35 -0300

    return a random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,9 @@ func main() {
 	log.SetPrefix( "greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	greetings.Init()
+
+	message, err := greetings.Hello("Flayshon")
 	if err != nil {
 		log.Fatal(err)
 	}
